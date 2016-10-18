a20cde45e5ee79e8c68953bb36a35c2c35e199d6
五环小胖子 <zer0131@vip.qq.com>
2016-10-19 01:23:17 +0800

    添加前端文件

---
+++ websrc/webpack.config.js
@@ -0,0 +1,56 @@
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        login: './src/login.js'
+    },
+    output: {
+        filename: '[name].min.js',
+        //chunkFilename: '[name].chunk.js',
+        path: '../app/public/dist',
+        publicPath: '/dist/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+                // query: {
+                //     presets: ['es2015', 'stage-0', 'react'],
+                //     'plugins': [
+                //         ['antd', {
+                //             style: 'css'
+                //         }]
+                //     ]
+                // }
+            },
+            {
+                test: /\.css$/,
+                // loader: "style!css"
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+            }
+        ]
+    },
+    plugins: [
+        // 第三方库打包生成的文件
+        //new webpack.optimize.CommonsChunkPlugin('vendors', 'vendor.js'),
+        // 压缩
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+        // production方式打包
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify('production')
+            }
+        }),
+        // 分离css
+        new ExtractTextPlugin("[name].min.css"),
+        new webpack.optimize.OccurenceOrderPlugin()
+    ]
+};
